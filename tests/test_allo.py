e005714aef8270b9e0688bda5d7668e33266a85d
Clement _dhwty_ <clemwek@gmail.com>
2017-05-15 16:07:25 +0300

    Add setup function to initialize classes

--- tests/test_allo.py
+++ tests/test_allo.py
@@ -3,31 +3,35 @@ from allo.allo import Dojo
 
 
 class TestCreateRoom(unittest.TestCase):
+
+    def setUp(self):
+        self.my_dojo = Dojo
+
     def test_create_room_successfully(self):
-        my_dojo = Dojo()
-        initial_room_count = len(my_dojo.all_rooms)
-        blue_office = my_dojo.create_room('Blue', 'office')
+        initial_room_count = len(self.my_dojo.all_rooms)
+        blue_office = self.my_dojo.create_room('Blue', 'office')
         self.assertTrue(blue_office)
-        new_room_count = len(my_dojo.all_rooms)
+        new_room_count = len(self.my_dojo.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
 
     def test_room_added(self):
-        my_dojo = Dojo()
-        self.assertFalse('Red' in my_dojo.all_rooms)
-        my_dojo.create_room('Red', 'office')
-        self.assertTrue('Red' in my_dojo.all_rooms)
+        self.assertFalse('Red' in self.my_dojo.all_rooms)
+        self.my_dojo.create_room('Red', 'office')
+        self.assertTrue('Red' in self.my_dojo.all_rooms)
 
 
 class TestAddPerson(unittest.TestCase):
+
+    def setUp(self):
+        self.my_dojo = Dojo
+
     def test_add_person_successfully(self):
-        my_dojo = Dojo()
-        initial_persons_count = len(my_dojo.all_persons)
-        my_dojo.add_person('Mike', 'fellow')
-        new_persons_count = len(my_dojo.all_persons)
+        initial_persons_count = len(self.my_dojo.all_persons)
+        self.my_dojo.add_person('Mike', 'fellow')
+        new_persons_count = len(self.my_dojo.all_persons)
         self.assertEqual(new_persons_count - initial_persons_count, 1)
 
     def test_room_added(self):
-        my_dojo = Dojo()
-        self.assertFalse('Fred' in my_dojo.all_persons)
-        my_dojo.add_person('Fred', 'fellow')
-        self.assertTrue('Fred' in my_dojo.all_persons)
+        self.assertFalse('Fred' in self.my_dojo.all_persons)
+        self.my_dojo.add_person('Fred', 'fellow')
+        self.assertTrue('Fred' in self.my_dojo.all_persons)
