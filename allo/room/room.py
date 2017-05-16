ff8cb3f3fcd1a99a41259064c85f1dbdcd484a65
Clement _dhwty_ <clemwek@gmail.com>
2017-05-16 11:37:46 +0300

    Add empty classes

--- allo/room/room.py
+++ allo/room/room.py
@@ -3,3 +3,16 @@
 """
 the Room class and its children are contained here
 """
+
+
+class Room(object):
+    def __init__(self, capacity=0):
+        self.capacity = capacity
+
+
+class Office(Room):
+    pass
+
+
+class LivingSpace(Room):
+    pass
