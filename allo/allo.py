8849cf0f977e62aec93f23448a8cbe835259f88c
Clement _dhwty_ <clemwek@gmail.com>
2017-05-16 11:36:55 +0300

    Updated the dojo class

--- allo/allo.py
+++ allo/allo.py
@@ -1,3 +1,45 @@
-class Dojo(object):
-    pass
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file serve as a starting point for a Python console script.
+"""
 
+
+class Dojo(object):
+    """
+    Class Dojo the main entry point to a110-cli
+    """
+    def __init__(self):
+        self.all_persons = []
+        self.all_fellows = []
+        self.all_staff = []
+        self.all_rooms = []
+        self.all_offices = []
+        self.all_living_space = []
+        self.room_allocation = {
+            "office": {},
+            "living_space": {}
+        }
+
+    def create_room(self, room_type, room_name):
+        room_type = room_type.lower()
+        room_name = room_name.lower()
+        if room_name in self.all_rooms:
+            return "Sorry, {} room name has been taken.".format(room_name)
+        if room_type == 'office':
+
+            self.all_rooms.append(room_name)
+            self.room_allocation['office'][room_name] = []
+            return "{} - office added successfully".format(room_name)
+        elif room_type == 'living':
+            self.all_rooms.append(room_name)
+            self.room_allocation['living_space'][room_name] = []
+            return "{} - living space added successfully".format(room_name)
+        else:
+            return "A room can only be office or living space"
+
+    def run(self):
+        self.create_room('Red', 'office')
+
+if __name__ == "__main__":
+    Dojo.run()
