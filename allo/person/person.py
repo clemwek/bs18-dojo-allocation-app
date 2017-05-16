98e53574b8b0d8c057e627b6d51ed34683bec01e
Clement _dhwty_ <clemwek@gmail.com>
2017-05-16 11:37:30 +0300

    Add empty classes

--- allo/person/person.py
+++ allo/person/person.py
@@ -3,3 +3,16 @@
 """
 the Person class and its children are contained here
 """
+
+
+class Person(object):
+    def add_person(self):
+        pass
+
+
+class Fellow(Person):
+    pass
+
+
+class Staff(Person):
+    pass
