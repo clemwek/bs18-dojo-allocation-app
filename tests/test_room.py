2efae7593cffb887fdb0f1837247ecdb8fb99361
Clement _dhwty_ <clemwek@gmail.com>
2017-05-17 10:53:20 +0300

    Updated tests for room

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,29 +1,10 @@
 import  unittest
-from allo.room import room
-from allo.person import person
+from allo.room.room import Room, Office, LivingSpace
 
 
 class TestRoom(unittest.TestCase):
     def test_can_be_created(self):
-        new_room = room.Room()
-        self.assertIsInstance(new_room, room.Room)
-
-    def test_capacity(self):
-        office = room.Room(4)
-        living_space = room.Room(6)
-        self.assertEqual(office.capacity, 4)
-        self.assertEqual(living_space.capacity, 6)
-
-    def test_occupants(self):
-        fellow = person.Fellow()
-        staff =person.Staff()
-
-    def test_add_office(self):
-        office = room.Office()
-        new_office = office.create_office('red', 'office')
-        self.assertIn('red', new_office)
-
-    def test_add_living_space(self):
-        living_space = room.LivingSpace()
-        new_living_space = living_space.create_living_space('green', 'living')
-        self.assertIn('green', new_living_space)
+        new_office = Office('Mango')
+        new_living_space = LivingSpace('Banana')
+        self.assertIsInstance(new_office, Office)
+        self.assertIsInstance(new_living_space, LivingSpace)
