9e1c8a13e1c55c9b9f221e55128f48a2818c1a10
Clement _dhwty_ <clemwek@gmail.com>
2017-05-17 10:52:48 +0300

    Updated tests for person

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,10 @@
 import unittest
-from allo.person import person
+from allo.person.person import Person, Fellow, Staff
 
 
 class TestPerson(unittest.TestCase):
     def test_can_be_created(self):
-        new_person = person.Person()
-        self.assertIsInstance(new_person, person.Person)
-
+        new_fellow = Fellow('Mike')
+        new_staff = Staff('jane')
+        self.assertIsInstance(new_fellow, Fellow)
+        self.assertIsInstance(new_staff, Staff)
