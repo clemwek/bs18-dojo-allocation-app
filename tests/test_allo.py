ad8c17a628e7c9899392c60d1a17f8483c48845b
Clement _dhwty_ <clemwek@gmail.com>
2017-05-17 21:26:41 +0300

    Added random allocation functionality tests

--- tests/test_allo.py
+++ tests/test_allo.py
@@ -31,7 +31,15 @@ class TestDojo(unittest.TestCase):
         self.assertTrue('mike' in self.my_dojo.all_persons.keys())
 
     def test_room_cant_allocated_more_than_capacity(self):
-        # Make an entry
-        capacity = self.my_dojo.all_rooms['red'].capacity
-        members = self.my_dojo.all_rooms['red'].members
+        mango = self.my_dojo.create_room('office', 'Mango')
+        self.assertTrue(mango)
+        self.my_dojo.allocate_person_room('Mike', 'mango')
+        self.my_dojo.allocate_person_room('James', 'mango')
+        self.my_dojo.allocate_person_room('penny', 'mango')
+        self.my_dojo.allocate_person_room('peter', 'mango')
+        self.my_dojo.allocate_person_room('Jane', 'mango')
+        self.my_dojo.allocate_person_room('Robin', 'mango')
+        self.my_dojo.allocate_person_room('Margret', 'mango')
+        capacity = self.my_dojo.all_rooms['mango'].capacity
+        members = len(self.my_dojo.all_rooms['mango'].members)
         self.assertGreater(capacity, members)
