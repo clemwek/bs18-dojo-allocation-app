414ebe542e4bc3bdf0ff749151c4a024fbd23df2
Clement _dhwty_ <clemwek@gmail.com>
2017-05-19 04:15:15 +0300

    Removed un neccesary lines

--- allo/room/room.py
+++ allo/room/room.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 class Room(object):
     def __init__(self, room_name, room_type, capacity=0):
         self.capacity = capacity
@@ -14,10 +10,8 @@ class Room(object):
 class Office(Room):
     def __init__(self, room_name):
         super(Office, self).__init__(room_name, room_type='office', capacity=6)
-        self.room_name = room_name
 
 
 class LivingSpace(Room):
     def __init__(self, room_name):
         super(LivingSpace, self).__init__(room_name, room_type='office', capacity=4)
-        self.room_name = room_name
