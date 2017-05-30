8084f11d64c129e2282221579355269b5d7ac278
Clement _dhwty_ <clemwek@gmail.com>
2017-05-30 15:19:33 +0300

    Update test for removed duplicate data structure

--- tests/test_allo.py
+++ tests/test_allo.py
@@ -8,27 +8,27 @@ class TestDojo(unittest.TestCase):
         self.my_dojo = Dojo()
 
     def test_create_room_successfully(self):
-        initial_room_count = len(self.my_dojo.all_rooms)
+        initial_office_count = len(self.my_dojo.all_rooms['office'])
         blue_office = self.my_dojo.create_room('office', 'Blue')
         self.assertTrue(blue_office)
-        new_room_count = len(self.my_dojo.all_rooms)
-        self.assertEqual(new_room_count - initial_room_count, 1)
+        new_office_count = len(self.my_dojo.all_rooms['office'])
+        self.assertEqual(new_office_count - initial_office_count, 1)
 
     def test_room_added(self):
-        self.assertFalse('red' in self.my_dojo.all_rooms)
+        self.assertFalse('red' in self.my_dojo.all_rooms['office'])
         self.my_dojo.create_room('office', 'Red')
-        self.assertTrue('red' in self.my_dojo.all_rooms.keys())
+        self.assertTrue('red' in self.my_dojo.all_rooms['office'].keys())
 
     def test_add_person_successfully(self):
-        initial_persons_count = len(self.my_dojo.all_persons)
+        initial_fellow_count = len(self.my_dojo.all_persons['fellow'])
         self.my_dojo.add_person('Mike', 'fellow')
-        new_persons_count = len(self.my_dojo.all_persons)
-        self.assertEqual(new_persons_count - initial_persons_count, 1)
+        new_fellow_count = len(self.my_dojo.all_persons['fellow'])
+        self.assertEqual(new_fellow_count - initial_fellow_count, 1)
 
     def test_person_added(self):
-        self.assertFalse('mike' in self.my_dojo.all_persons)
+        self.assertFalse('mike' in self.my_dojo.all_persons['fellow'])
         self.my_dojo.add_person('Mike', 'Fellow')
-        self.assertTrue('mike' in self.my_dojo.all_persons.keys())
+        self.assertTrue('mike' in self.my_dojo.all_persons['fellow'].keys())
 
     def test_print_room(self):
         if self.my_dojo.create_room('office', 'Mango'):
@@ -41,20 +41,19 @@ class TestDojo(unittest.TestCase):
         self.assertEqual(self.my_dojo.print_allocations(), 'There are no rooms and no allocations made yet!')
         if self.my_dojo.create_room('office', 'Mango'):
             self.my_dojo.add_person('Mike', 'staff')
-            print(self.my_dojo.all_rooms['mango'].members)
-            self.assertIn('mike', self.my_dojo.all_rooms['mango'].members)
+            self.assertIn('mike', self.my_dojo.all_rooms['office']['mango'].members)
 
     def test_unallocated(self):
         self.my_dojo.add_person('Mike', 'staff')
-        self.assertFalse(self.my_dojo.all_persons['mike'].accommodation)
+        self.assertFalse(self.my_dojo.all_persons['staff']['mike'].accommodation)
 
     def test_list_rooms(self):
-        self.assertEqual(len(self.my_dojo.all_rooms), 0)
+        self.assertEqual(len(self.my_dojo.all_rooms['office']), 0)
+        self.assertEqual(len(self.my_dojo.all_rooms['living_space']), 0)
         self.my_dojo.create_room('office', 'Mango')
         self.my_dojo.create_room('living', 'Banana')
-        print(self.my_dojo.all_rooms)
-        self.assertIn('mango', self.my_dojo.list_rooms())
-        self.assertIn('banana', self.my_dojo.list_rooms())
+        self.assertIn('mango', self.my_dojo.list_rooms('office'))
+        self.assertIn('banana', self.my_dojo.list_rooms('living'))
 
     def test_rand_office_gen(self):
         pass
@@ -64,14 +63,14 @@ class TestDojo(unittest.TestCase):
         self.my_dojo.create_room('living', 'pine')
         self.my_dojo.add_person('john doe', 'staff', 'y')
         self.my_dojo.add_person('pete bet', 'fellow', 'y')
-        self.assertIn('john doe', self.my_dojo.all_rooms['mango'].members)
-        self.assertIn('pete bet', self.my_dojo.all_rooms['pine'].members)
+        self.assertIn('john doe', self.my_dojo.all_rooms['office']['mango'].members)
+        self.assertIn('pete bet', self.my_dojo.all_rooms['living_space']['pine'].members)
         self.my_dojo.create_room('office', 'banana')
         self.my_dojo.create_room('living', 'apple')
         self.my_dojo.reallocate_person('john doe', 'banana')
         self.my_dojo.reallocate_person('pete bet', 'apple')
-        self.assertIn('john doe', self.my_dojo.all_rooms['banana'].members)
-        self.assertIn('pete bet', self.my_dojo.all_rooms['apple'].members)
+        self.assertIn('john doe', self.my_dojo.all_rooms['office']['banana'].members)
+        self.assertIn('pete bet', self.my_dojo.all_rooms['living_space']['apple'].members)
 
     def test_load_people(self):
         print_file = 'OLUWAFEMI SULE FELLOW Y \nDOMINIC WALTERS STAFF '
@@ -79,5 +78,16 @@ class TestDojo(unittest.TestCase):
         f.write(print_file)
         f.close()
         self.my_dojo.load_people('test.txt')
-        print(self.my_dojo.all_persons.keys())
-        self.assertIn('OLUWAFEMI SULE'.lower(), self.my_dojo.all_persons.keys())
+        print(self.my_dojo.all_persons['fellow'].keys())
+        self.assertIn('OLUWAFEMI SULE'.lower(), self.my_dojo.all_persons['fellow'].keys())
+        self.assertIn('DOMINIC WALTERS'.lower(), self.my_dojo.all_persons['staff'].keys())
+
+    def test_check_space_in_room(self):
+        self.my_dojo.create_room('office', 'mango')
+        self.my_dojo.add_person('mercy', 'staff')
+        self.my_dojo.add_person('tom', 'staff')
+        self.my_dojo.add_person('mary', 'staff')
+        self.my_dojo.add_person('john', 'staff')
+        self.my_dojo.add_person('jane', 'staff')
+        self.my_dojo.add_person('tim', 'staff')
+        self.assertFalse(self.my_dojo.check_space_in_room('mango', 'office'))
